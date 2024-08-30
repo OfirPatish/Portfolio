711874a80effd4e377d27bac82e14d370fbef5bf
Ofir <ofirpatishop@gmail.com>
2024-08-30 14:35:04 +0300

    Removed unused import.

--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import { MdEmail } from "react-icons/md";
 import Lottie from "lottie-react";
 import envelopeAnimation from "../assets/Envelope.json";
 
